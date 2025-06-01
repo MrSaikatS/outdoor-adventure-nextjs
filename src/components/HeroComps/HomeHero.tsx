8c0ac1ff5ae218bba40080bdff4ff70b2a1e6f7f
Saikat Sardar <saikatsardar5@gmail.com>
2025-06-01 14:13:07 +0530

    Create HomeHero.tsx

---
+++ src/components/HeroComps/HomeHero.tsx
@@ -0,0 +1,15 @@
+const HomeHero = () => {
+  return (
+    <>
+      <section className="bg-[url(/home/hero-bg.jpg)] bg-center">
+        <div className="flex flex-col items-center justify-center bg-black/50 py-40 text-white">
+          <div className="">Explore the Colourful World</div>
+
+          <div className="bg-primary h-0.5 w-4"></div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HomeHero;
