2ff6e0f237331358b6f06263040444c288181479
Saikat Sardar <saikatsardar5@gmail.com>
2025-06-01 13:08:06 +0530

    project init 🚀

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+import "./globals.css";
+
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+const RootLayout = ({ children }: RootLayoutProps) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
